b3eed20676516b7e4034a927409284f2e7af2517
Conrad hindman <59845602+Conradmh1026@users.noreply.github.com>
2020-02-14 14:51:06 -0500

    Working test

--- src/response.js
+++ src/response.js
@@ -1,49 +1,96 @@
+
+const Users = {};
+
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+
+//gets a user
+const getUsers = (request, response) => {
+  const responseJSON = {
+    Users,
   };
-  
-  //200
-  const success = (request, response) => {
-    const responseJSON = {
-      message: 'This is a successful response',
-      id: 'Success',
-    };
-  
-    respondJSON(request, response, 200, responseJSON);
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+
+//add a user
+const addUser = (request, response, body) => {
+  const responseJSON= {
+    message: 'Name and age are both required',
   };
-  
-  //400
-  const badRequest = (request, response, params) => {
-    const responseJSON = {
-      message: 'This request has the required parameters',
-    };
-  
-    if(!params.valid || params.valid !== 'true')
-    {
-      responseJSON.message = 'Missing valid query parameter set to true',
-      responseJSON.id = 'badRequest';
-  
-      return respondJSON(request, response, 400 , responseJSON);
-    }
-    return  respondJSON(request, response, 200 , responseJSON);
-  
+  if(!body.name || !body.age)
+  {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if(Users[body.name]){
+    responseCode = 204;
+  }else{
+    Users[body.name] = {};
+  }
+
+  Users[body.name].name = body.name;
+  Users[body.name].age = body.age;
+
+  if(responseCode === 201){
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response,responseCode);
+};
+
+
+
+/* 200
+const success = (request, response) => {
+  const responseJSON = {
+    message: 'This is a successful response',
+    id: 'Success',
   };
-  
-  //404
-  const notFound = (request, response) => {
-    const responseJSON = {
-      message: 'The page you are looking for was not found.',
-      id: 'notFound',
-    };
-  
-    respondJSON(request,response, 404 ,responseJSON);
-  
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+// 400
+const badRequest = (request, response, params) => {
+  const responseJSON = {
+    message: 'This request has the required parameters',
   };
-  
-  module.exports = {
-    success,
-    badRequest,
-    notFound,
+
+  if (!params.valid || params.valid !== 'true') {
+    responseJSON.message = 'Missing valid query parameter set to true',
+    responseJSON.id = 'badRequest';
+
+    return respondJSON(request, response, 400, responseJSON);
+  }
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+// 404
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
   };
+
+  respondJSON(request, response, 404, responseJSON);
+};*/
+
+module.exports = {
+  getUsers,
+  addUser,
+};
--- src/server.js
+++ src/server.js
@@ -1,33 +1,64 @@
 const http = require('http');
 const url = require('url');
 const query = require('querystring');
-const htmlHandler = require('./htmlResponses.js');
-const jsonHandler = require('./jsonResponses.js');
+const htmlHandler = require('./htmlResponse.js');
+const jsonHandler = require('./response.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const urlStruct = {
+const handlePost = (request, response, parsedUrl) => {
+  if(parsedUrl.pathname === '/addUser'){
+    const body = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+    request.on('data', (chunk) =>{
+      body.push(chunk);
+    });
+
+    request.on('end', ()=>{
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      jsonHandler.addUser(request, response, bodyParams);
+    });
+
+  }
+};
+
+const handleGet = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCss(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
+};
+
+/*const urlStruct = {
   '/': htmlHandler.getIndex,
   '/success': jsonHandler.success,
   '/badRequest': jsonHandler.badRequest,
+  '/style': htmlHandler.getCss,
   notFound: jsonHandler.notFound,
-};
+};*/
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-    const params = query.parse(parsedUrl.query);
-  
-    console.dir(request.url);
-    console.dir(parsedUrl);
-    console.dir(params);
-  
-    if(urlStruct[parsedUrl.pathname])
-    {
-      urlStruct[parsedUrl.pathname](request,response,params);
-    }
-    else{
-      urlStruct.notFound(request,response,params);
-    }
-  };
-  
-  http.createServer(onRequest).listen(port);
+  const parsedUrl = url.parse(request.url);
+
+  if(request.method === 'POST')
+  {
+    handlePost(request, response, parsedUrl);
+  }
+  else{
+    handleGet(request,response, parsedUrl);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
